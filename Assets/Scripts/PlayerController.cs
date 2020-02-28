e468d4379249b4d748e3ac1f3e05c4b26983404d
hinode000jp <takenoko000jp@gmail.com>
2020-02-28 19:30:57 +0900

    update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,4 +21,7 @@ using UnityEngine;
     }
 
     private void FixedUpdate()    {        Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;        Vector3 moveForward = cameraForward * inputVertical + Camera.main.transform.right * inputHorizontal;        rb.velocity = moveForward * moveSpeed + new Vector3(0, rb.velocity.y, 0);        if (moveForward != Vector3.zero)        {            transform.rotation = Quaternion.LookRotation(moveForward);        }    }
+
+    private void OnTriggerEnter(Collider other)    {        if (other.CompareTag("Crystal"))        {            Destroy(other.gameObject);        }
+    }
 }
